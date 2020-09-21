af3bbcaaf833593b3f505266abd49839e2706ec0
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2020-09-21 08:13:40 +0100

    added heroku client

---
+++ pythonClient/client-heroku.py
@@ -0,0 +1,55 @@
+import requests
+import threading
+import json
+
+globToken = ''
+globRefreshKey = ''
+device_id='Test_HARDWARE'
+url='https://pat-code.herokuapp.com'
+
+
+def getToken(device_id):
+    global globToken
+    global globRefreshKey
+    response = requests.post(
+        url+'/generate_key', data={'device_id': device_id})
+    val = json.loads(response.text)
+    globToken = val["access_token"]
+    globRefreshKey = val["refresh_token"]
+
+
+def refreshToken(token):
+    global globToken
+    global globRefreshKey
+    response = requests.post(
+        url+'/refresh_key',
+        headers={'Authorization': 'Bearer ' + token})
+    val = json.loads(response.text)
+    print('\nNew Token Fetched ')
+    globToken = val["access_token"]
+    globRefreshKey = val["refresh_token"]
+
+
+def sendData(payload, token):
+    print('\n Posting Data: ' + str(payload))
+    response = requests.post(url+'/data', data=payload,
+                             headers={'Authorization': 'Bearer ' + token})
+    print(response.text)
+
+
+def sendDataTimer(interval):
+    threading.Timer(interval, sendDataTimer, args=(interval,)).start()
+    sendData({'amount': 1234, 'currency':'BTC'}, globToken)
+
+
+def refreshTokenTimer(interval):
+    threading.Timer(interval, refreshTokenTimer, args=(interval,)).start()
+    print("\n RefreshToken Job Begins!!!")
+    refreshToken(globRefreshKey)
+
+#fetching initial Token for Authentication
+getToken(device_id)
+#Starting Refresh Token faux_CronJob (runs every 15 seconds)
+refreshTokenTimer(15)
+#Starting Refresh Token faux_CronJob (runs every 4 seconds)
+sendDataTimer(4)
